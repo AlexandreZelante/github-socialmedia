984b8c0f3ce151d423ffd9a3465a9e782950106b
Alexandre Zelante <alexandre.zelante@outlook.com>
2020-02-20 18:03:15 -0300

    add

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes.js");
+
+const app = express();
+
+mongoose.connect(
+    "mongodb+srv://omnistack:omnistack@omnistack-6jh9d.mongodb.net/test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+app.use(express.json());
+
+//Usa o Router do express
+app.use(routes);
+
+//Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+
+//Query Params: req.query (Filtros, ordenação, páginação, ...)
+//Route Params: req.params (Identificar um recurso na alteração ou remoção)
+//Body: req.body (Dados para criação ou alteração de um registro)
+
+app.listen(3333);
